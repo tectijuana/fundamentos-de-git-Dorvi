f58f12b01615559d574ab18568f7a7934ea9a45e
David Páez <96804874+DavidPaBe@users.noreply.github.com>
2023-04-17 16:45:26 -0700

    Create Ejercicio 01.cpp

---
+++ Unidad 2/2.7/Ejercicio 01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// Ejercicio No. 1
+// Problema No. 1. Capítulo 1.
+// Imprimir los números enteros del 9 al 43
+
+//@Autor        Páez Beltrán David
+//@Fecha        17/04/2023
+
+using namespace std;
+
+int main() {
+
+        int i = 9;
+        for(i = 9; i <= 43; i++){
+                cout << i << "\r\n";
+}
+        return 0;
+}
